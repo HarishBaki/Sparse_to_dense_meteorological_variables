e9260348fd9bbf0a578aeda2e652dc81bc5f6a3b
HarishBaki <harish.iiitn@gmail.com>
2025-04-21 14:43:12 -0400

    Add Deep Convolutional Neural Network (DCNN) implementation.

---
+++ models/Deep_CNN.py
@@ -0,0 +1,59 @@
+# %%
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# %%
+class DCNN(nn.Module):
+    '''
+    Deep Convolutional Neural Network (DCNN) for image processing tasks.
+    This model consists of multiple convolutional layers followed by ReLU activations.
+    Parameters:
+    - in_channels (int): Number of input channels.
+    - out_channels (int): Number of output channels.
+    - C (int): Number of channels in the intermediate layers.
+    - kernel (tuple): Size of the convolutional kernel.
+    - final_kernel (tuple): Size of the final convolutional kernel.
+    - n_layers (int): Number of convolutional layers in the network.
+    Returns:
+    - output (Tensor): Output tensor after passing through the network.
+    '''
+    def __init__(self, in_channels, out_channels, C=48,kernel=(7,7),final_kernel=(3,3),n_layers=7):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.C = C
+        self.kernel = kernel
+        self.final_kernel = final_kernel    
+        self.n_layers = n_layers
+        self.blocks = nn.ModuleList()
+        for i in range(n_layers):
+            if i == 0:
+                in_channels = self.in_channels
+                out_channels = self.C
+            else:
+                in_channels = self.C
+                out_channels = self.C
+            self.blocks.append(nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=kernel, padding=(kernel[0] // 2, kernel[1] // 2)),
+                nn.ReLU(inplace=True)
+            ))
+        self.final_conv = nn.Conv2d(C, self.out_channels, kernel_size=self.final_kernel, padding=1)
+
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        x = self.final_conv(x)
+        return x
+# %%
+if __name__ == "__main__":
+    # Example usage
+    model = DCNN(in_channels=3, out_channels=1, C=48, kernel=(7, 7),final_kernel=(3,3), n_layers=7)
+    print(model)  # Print the model architecture
+    x = torch.randn(1, 3, 256, 288)  
+    output = model(x)
+    print(output.shape)
+    '''
+    Already examined the model architecture, intermediate outputs shape and the final output shape.
+    The shape is retained from from end to end.
+    '''  
