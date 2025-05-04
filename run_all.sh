819474b45551d3e3cc200f625487343d2483a6c5
HarishBaki <harish.iiitn@gmail.com>
2025-05-04 22:43:19 +0000

    Rerun the test output for jobs that were corrupted due to mistake rclone. Updated the results.ipynb.

--- run_all.sh
+++ run_all.sh
@@ -2,14 +2,15 @@
 # DCNN and all variables are not submitted. 
 variables=("si10" "t2m" "sh2")
 models=("DCNN" "UNet" "SwinT2UNet")
-orographies=("false" "true")
+orography_as_channels=("false" "true")
 add_vars=("si10" "si10,t2m" "si10,t2m,sh2")
 years=("2018" "2018,2019" "2018,2020")
 n_stations=("50" "75" "100")
 
 for model in "${models[@]}"; do
   for variable in "${variables[@]}"; do
-      export variable model
-      sbatch --export=All python_jobsub.slurm   # bash python_jobsub.slurm for bash script.
+      export model variable
+      #sbatch --export=All python_jobsub.slurm   
+      bash python_jobsub.slurm
   done
 done
