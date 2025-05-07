2fac7b7fa1ccb4675a56549fb095ec9c4bd9d528
HarishBaki <harish.iiitn@gmail.com>
2025-05-07 22:42:02 +0000

    Updated the UNet with dropout_prob, drop_path_prob, and act_layer parameters. Added weight initialization with seed. Checked the initialized weights.

--- models/UNet.py
+++ models/UNet.py
@@ -3,22 +3,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from timm.layers import DropPath
+from util import initialize_weights_xavier,initialize_weights_He
 
 # %%
 class ConvBlock(nn.Module):
-    def __init__(self, in_channel, out_channel, strides=1,dropout_prob=0.2,drop_path_prob=0.0):
+    def __init__(self, in_channel, out_channel,dropout_prob=0.2,drop_path_prob=0.0,act_layer=nn.ReLU):
         super(ConvBlock, self).__init__()
-        self.strides = strides
-        self.in_channel=in_channel
-        self.out_channel=out_channel
         self.block = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=strides, padding=1),
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=strides, padding=1),
-            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=1, padding=1),
+            act_layer(),
+            nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1),
+            act_layer(),
             nn.Dropout2d(p=dropout_prob)
         )
-        self.conv11 = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=strides, padding=0)
+        self.conv11 = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=1, padding=0)
         self.drop_path = DropPath(drop_path_prob) if drop_path_prob > 0.0 else nn.Identity()
 
     def forward(self, x):
@@ -29,7 +27,7 @@ class ConvBlock(nn.Module):
         return out
     
 class Encoder(nn.Module):
-    def __init__(self,in_channels, C, n_layers=4):
+    def __init__(self,in_channels, C, dropout_prob=0.2,drop_path_prob=0.0,act_layer=nn.ReLU, n_layers=4):
         super().__init__()
         self.in_channels = in_channels
         self.C = C
@@ -44,7 +42,7 @@ class Encoder(nn.Module):
             else:
                 in_channels = self.C*(2**(i-1))
                 out_channels = self.C*(2**i)
-            self.blocks.append(ConvBlock(in_channels, out_channels))
+            self.blocks.append(ConvBlock(in_channels, out_channels,dropout_prob=dropout_prob,drop_path_prob=drop_path_prob,act_layer=act_layer))
             self.downs.append(nn.Conv2d(out_channels, out_channels, kernel_size=4, stride=2, padding=1))
 
     def forward(self, x):
@@ -58,13 +56,13 @@ class Encoder(nn.Module):
         return x, skip_connections
 
 class Bottleneck(nn.Module):
-    def __init__(self, C, n_layers=4):
+    def __init__(self, C,dropout_prob=0.2,drop_path_prob=0.0,act_layer=nn.ReLU, n_layers=4):
         super().__init__()
         self.C = C
         self.n_layers = n_layers
         in_channels = self.C*(2**(n_layers-1))
         out_channels = self.C*(2**n_layers)
-        self.block = ConvBlock(in_channels, out_channels)
+        self.block = ConvBlock(in_channels, out_channels,dropout_prob=dropout_prob,drop_path_prob=drop_path_prob,act_layer=act_layer)
 
     def forward(self, x):
         x = self.block(x)
@@ -72,7 +70,7 @@ class Bottleneck(nn.Module):
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, out_channels, C, n_layers=4):
+    def __init__(self, out_channels, C,dropout_prob=0.2,drop_path_prob=0.0,act_layer=nn.ReLU, n_layers=4):
         super().__init__()
         self.out_channels = out_channels
         self.C = C
@@ -84,7 +82,7 @@ class Decoder(nn.Module):
             in_channels = self.C*(2**(i+1))
             out_channels = self.C*(2**(i))
             self.ups.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2))
-            self.blocks.append(ConvBlock(in_channels, out_channels, strides=1))
+            self.blocks.append(ConvBlock(in_channels, out_channels,dropout_prob=dropout_prob,drop_path_prob=drop_path_prob,act_layer=act_layer))
         self.blocks.append(nn.Conv2d(self.C, self.out_channels, kernel_size=1, stride=1, padding=0))
 
     def forward(self, x, skip_connections):
@@ -118,12 +116,12 @@ class UNet(nn.Module):
     In the decoder, the spatial dimensions will be doubled after each upsampling layer, then the channels will double by concatnation, while the channels will be halved after each ConvBlock.
 
     '''
-    def __init__(self, in_channels=3, out_channels=1, C=32, n_layers=4,hard_enforce_stations=False):
+    def __init__(self, in_channels=3, out_channels=1, C=32, dropout_prob=0.2,drop_path_prob=0.0,act_layer=nn.ReLU, n_layers=4,hard_enforce_stations=False):
         super(UNet, self).__init__()
         self.hard_enforce_stations = hard_enforce_stations
-        self.encoder = Encoder(in_channels, C, n_layers)
-        self.bottleneck = Bottleneck(C, n_layers)
-        self.decoder = Decoder(out_channels, C, n_layers)
+        self.encoder = Encoder(in_channels, C,dropout_prob,drop_path_prob,act_layer, n_layers)
+        self.bottleneck = Bottleneck(C, dropout_prob,drop_path_prob,act_layer, n_layers)
+        self.decoder = Decoder(out_channels, C, dropout_prob,drop_path_prob,act_layer, n_layers)
 
     def forward(self, x):
         if self.hard_enforce_stations:
@@ -138,14 +136,24 @@ class UNet(nn.Module):
     
 # %%    
 if __name__ == "__main__":
-    model = UNet(in_channels=3, out_channels=1, C=32, n_layers=4,hard_enforce_stations=True)
+    act_layer = nn.ReLU
+    seed = 42
+    model = UNet(in_channels=3, out_channels=1, C=32, dropout_prob=0.2,drop_path_prob=0.2,act_layer=act_layer,n_layers=4,hard_enforce_stations=True)
+    if act_layer == nn.GELU:
+        initialize_weights_xavier(model,seed = seed)
+    elif act_layer == nn.ReLU:
+        initialize_weights_He(model,seed = seed)
     print(model)
     # print the total number of parameters in the model
     print(f'Total number of parameters: {sum(p.numel() for p in model.parameters())}')
     x = torch.randn(1, 3, 256, 288)  # Example input
     output = model(x)
-    print("Output shape:", output.shape)  # Should be (1, 1, 256, 256)
+    print("Output shape:", output.shape)  # Should be (1, 1, 256, 2)
     '''
     Already examined the model architecture, intermediate outputs shape and the final output shape.
     ''' 
+    # Print weights of the first conv layer
+    first_conv_layer = model.encoder.blocks[0].block[0]  # First nn.Conv2d inside the first block
+    print("Weights of the first Conv2d layer:")
+    print(first_conv_layer.weight.data)
 # %%
