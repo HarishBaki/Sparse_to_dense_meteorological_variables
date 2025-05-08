ff7418f2c7f2d035f45fe8923c1f0126cbeea6f0
HarishBaki <harish.iiitn@gmail.com>
2025-05-08 09:55:29 -0400

    Refactor training script and update job submission parameters
    - Updated `python_jobsub.slurm` to set default values for `nproc_per_node`, `model`, `weights_seed`, and `activation_layer`.
    - Added new parameters to the training command for weights seed and activation layer.
    - Modified `results.ipynb` to reset execution counts and update output timestamps.
    - Added a new code cell in `results.ipynb` for testing model performance with a station mask.
    - Refactored `run_all.sh` to correct variable names and ensure proper job submission for different configurations.

--- run_all.sh
+++ run_all.sh
@@ -3,14 +3,22 @@
 variables=("si10" "t2m" "sh2")
 models=("DCNN" "UNet" "SwinT2UNet")
 orography_as_channels=("false" "true")
-add_vars=("si10" "si10,t2m" "si10,t2m,sh2")
+additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
 years=("2018" "2018,2019" "2018,2020")
 n_stations=("50" "75" "100")
+weights_seeds=("43" "44" "45" "46")
 
-for model in "${models[@]}"; do
-  for variable in "${variables[@]}"; do
-      export model variable
-      #sbatch --export=All python_jobsub.slurm   
-      bash python_jobsub.slurm
-  done
+model='DCNN'
+#for model in "${models[@]}"; do
+for orography_as_channel in "${orography_as_channels[@]}"; do
+    export model orography_as_channel
+    sbatch --export=All python_jobsub.slurm   
+    #bash python_jobsub.slurm
+done
+
+orography_as_channel='true'
+for additional_input_variables in "${additional_input_variabless[@]}"; do
+    export model orography_as_channel additional_input_variables
+    sbatch --export=All python_jobsub.slurm   
+    #bash python_jobsub.slurm
 done
