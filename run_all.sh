1057b0d9dff59e074f8c3cb67695ed45c28b6987
HarishBaki <harish.iiitn@gmail.com>
2025-05-12 14:33:20 -0400

    Updated the run_all.sh variable names matching to that of the python_jobsub

--- run_all.sh
+++ run_all.sh
@@ -4,21 +4,15 @@ variables=("si10" "t2m" "sh2")
 models=("DCNN" "UNet" "SwinT2UNet")
 orography_as_channels=("false" "true")
 additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
-years=("2018" "2018,2019" "2018,2020")
-n_stations=("50" "75" "100")
+train_years_ranges=("2018" "2018,2019" "2018,2020")
+n_random_stationss=("50" "75" "100")
 weights_seeds=("43" "44" "45" "46")
 
-model='DCNN'
-#for model in "${models[@]}"; do
-for orography_as_channel in "${orography_as_channels[@]}"; do
-    export model orography_as_channel
-    sbatch --export=All python_jobsub.slurm   
-    #bash python_jobsub.slurm
-done
-
+model='UNet'
 orography_as_channel='true'
-for additional_input_variables in "${additional_input_variabless[@]}"; do
-    export model orography_as_channel additional_input_variables
+additional_input_variables='si10,t2m,sh2'
+for n_random_stations in "${n_random_stationss[@]}"; do
+    export model orography_as_channel additional_input_variables n_random_stations
     sbatch --export=All python_jobsub.slurm   
     #bash python_jobsub.slurm
 done
