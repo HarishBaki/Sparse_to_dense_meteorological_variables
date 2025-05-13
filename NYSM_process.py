66f77f43580e4cf6888e199c6cdeb28af5ba5e2f
HarishBaki <harish.iiitn@gmail.com>
2025-05-13 00:00:09 -0400

    Renamed the NYSM variables into RTMA compatiable.

--- NYSM_process.py
+++ NYSM_process.py
@@ -20,13 +20,23 @@ ds_subset = ds[['latitude','longitude',
          'temp_2m','relative_humidity','dewpoint','station_pressure',
          'avg_wind_speed_prop_sonic','max_wind_speed_prop_sonic',]]
 
+# Rename variables to match RTMA
+ds_subset = ds_subset.rename({
+    'temp_2m': 't2m',
+    'relative_humidity': 'rh2',
+    'dewpoint': 'd2m',
+    'station_pressure': 'sp',
+    'avg_wind_speed_prop_sonic': 'si10',
+    'max_wind_speed_prop_sonic': 'i10fg'
+})
+
 # Quantify variables with units
 ds_q = ds_subset.metpy.quantify()
 
 # Compute specific humidity
 ds_q['sh2'] = mpcalc.specific_humidity_from_dewpoint(
-    ds_q['station_pressure'],
-    ds_q['dewpoint']
+    ds_q['sp'],
+    ds_q['d2m']
 )
 
 # Dequantify if you want to remove unit annotations for further use
