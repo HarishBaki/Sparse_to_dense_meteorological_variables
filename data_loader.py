921606c3be9f99f5799fb4886b7220cb3481b21b
HarishBaki <harish.iiitn@gmail.com>
2025-05-13 00:00:43 -0400

    Added a script to check the nan time instances across stations and thresholding.

--- data_loader.py
+++ data_loader.py
@@ -387,3 +387,12 @@ if __name__ == "__main__":
     _, station_indices = tree.query(nysm_latlon)
     NYSM = NYSM.isel(station=station_indices)  # this is needed to match the nysm_latlon order
     NYSM = NYSM.sel(time=slice(dates_range[0], dates_range[1]))
+
+    NYSM = NYSM.sel(time=slice('2023-01-01T00:00', '2023-12-31T00:00'))
+    NYSM = NYSM.resample(time='1H').nearest()
+    for var in NYSM.data_vars:
+        for i in range(110,len(NYSM['station'].values)):
+            #i = 116
+            print(var, i, NYSM['time'].where(NYSM[var].notnull().sum(dim='station')<=i).dropna(dim='time').shape)
+
+
