9e06ef456b126d8b1bf2804ad1328986f1a752bf
HarishBaki <harish.iiitn@gmail.com>
2025-05-17 18:20:11 +0000

    Some inference outputs were not computed. They are done now.

--- run_all_inference.sh
+++ run_all_inference.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 n_random_stationss=("50" "75" "100" "none")
+n_inference_stationss=("50" "75" "100" "none")
 orography_as_channel='true'
 additional_input_variables='si10,t2m,sh2'
 train_years_range='2018,2021'
 for n_random_stations in "${n_random_stationss[@]}"; do
-    for n_inference_stations in "${n_random_stationss[@]}"; do
+    for n_inference_stations in "${n_inference_stationss[@]}"; do
         export orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations
         sbatch --export=All jobsub_inference.slurm
     done
