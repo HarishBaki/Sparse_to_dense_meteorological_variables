825e6e2a8072e4adb2a4636b85c74776458f13d2
HarishBaki <harish.iiitn@gmail.com>
2025-05-18 22:13:20 -0400

    Run all the metrics, including different variables.

--- run_all_metrics.sh
+++ run_all_metrics.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 variable='i10fg'
 data_types=("RTMA" "NYSM")
 models=("DCNN" "UNet" "SwinT2UNet")
 orography_as_channels=("false" "true")
 additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
-
+: '
 for data_type in "${data_types[@]}"; do
     for model in "${models[@]}"; do
         for orography_as_channel in "${orography_as_channels[@]}"; do
@@ -52,16 +53,18 @@ for data_type in "${data_types[@]}"; do
         done
     done
 done
-  
+'  
 model="UNet"
 variable='si10'
 orography_as_channel='true'
 additional_input_variables='i10fg,t2m,sh2'
 train_years_range='2018,2021'
+n_random_stations='none'
+n_inference_stations='none'
 for data_type in "${data_types[@]}"; do
-    export variable data_type model orography_as_channel additional_input_variables train_years_range
-    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range'
-    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range"
+    export variable data_type model orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations
+    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range|n_random_stations|n_inference_stations'
+    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range, n_random_stations: $n_random_stations, n_inference_stations: $n_inference_stations"
     sbatch --export=All jobsub_metrics.slurm
 done
 
@@ -70,10 +73,12 @@ variable='t2m'
 orography_as_channel='true'
 additional_input_variables='i10fg,si10,sh2'
 train_years_range='2018,2021'
+n_random_stations='none'
+n_inference_stations='none'
 for data_type in "${data_types[@]}"; do
-    export variable data_type model orography_as_channel additional_input_variables train_years_range
-    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range'
-    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range"
+    export variable data_type model orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations
+    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range|n_random_stations|n_inference_stations'
+    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range, n_random_stations: $n_random_stations, n_inference_stations: $n_inference_stations"
     sbatch --export=All jobsub_metrics.slurm
 done
 
@@ -82,9 +87,11 @@ variable='sh2'
 orography_as_channel='true'
 additional_input_variables='i10fg,si10,t2m'
 train_years_range='2018,2021'
+n_random_stations='none'
+n_inference_stations='none'
 for data_type in "${data_types[@]}"; do
-    export variable data_type model orography_as_channel additional_input_variables train_years_range
-    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range'
-    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range"
+    export variable data_type model orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations
+    export -p | grep -E 'variable|data_type|model|orography_as_channel|additional_input_variables|train_years_range|n_random_stations|n_inference_stations'
+    echo "Running for variable: $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables, train_years_range: $train_years_range, n_random_stations: $n_random_stations, n_inference_stations: $n_inference_stations"
     sbatch --export=All jobsub_metrics.slurm
 done
