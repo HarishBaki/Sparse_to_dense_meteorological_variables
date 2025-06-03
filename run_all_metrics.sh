76348d6dfce59df3f9b0ad2cfb6f1bb45ed74486
HarishBaki <harish.iiitn@gmail.com>
2025-06-03 08:15:00 -0400

    Metrics for the sensitivity of weights iniatilization are computed.

--- run_all_metrics.sh
+++ run_all_metrics.sh
@@ -5,25 +5,26 @@ data_types=("RTMA")
 models=("DCNN" "UNet" "SwinT2UNet")
 orography_as_channels=("false" "true")
 additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
-if false; then
+weights_seeds=("43" "44" "45" "46")
+for weights_seed in "${weights_seeds[@]}"; do
     for data_type in "${data_types[@]}"; do
         for model in "${models[@]}"; do
             for orography_as_channel in "${orography_as_channels[@]}"; do
-                export variable data_type model orography_as_channel
-                export -p | grep -E 'variable|data_type|model|orography_as_channel'
-                echo "Running for variable $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel"
+                export variable data_type model orography_as_channel weights_seed
+                echo "Running for variable $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel with weights_seed: $weights_seed"
                 sbatch --export=All jobsub_metrics.slurm
             done
             orography_as_channel='true'
             for additional_input_variables in "${additional_input_variabless[@]}"; do
-                export variable data_type model orography_as_channel additional_input_variables
-                export -p | grep -E 'variable|variable|data_type|model|orography_as_channel|additional_input_variables'
-                echo "Running for variable $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables"
+                export variable data_type model orography_as_channel additional_input_variables weights_seed
+                echo "Running for variable $variable, data_type: $data_type, model: $model, orography_as_channel: $orography_as_channel, additional_input_variables: $additional_input_variables with weights_seed: $weights_seed"
                 sbatch --export=All jobsub_metrics.slurm
             done
         done
     done
+done
 
+if false; then
     train_years_ranges=("2018" "2018,2019" "2018,2020")
     model="UNet"
     orography_as_channel='true'
@@ -38,26 +39,28 @@ if false; then
     done
 fi
 
-model="UNet"
-train_years_range='2018,2021'
-orography_as_channel='true'
-additional_input_variables='si10,t2m,sh2'
-n_random_stationss=("none" "50" "75" "100")
-n_inference_stationss=("50" "75" "100")
-randomize_stations_persamples=("true" "false")
-inference_stations_seeds=(43 44 45 46) # Different seeds for inference stations
-for data_type in "${data_types[@]}"; do
-    for inference_stations_seed in "${inference_stations_seeds[@]}"; do
-        for randomize_stations_persample in "${randomize_stations_persamples[@]}"; do
-            for n_random_stations in "${n_random_stationss[@]}"; do
-                for n_inference_stations in "${n_inference_stationss[@]}"; do
-                    export variable data_type model orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations randomize_stations_persample inference_stations_seed
-                    sbatch --export=All jobsub_metrics.slurm
+if false; then
+    model="UNet"
+    train_years_range='2018,2021'
+    orography_as_channel='true'
+    additional_input_variables='si10,t2m,sh2'
+    n_random_stationss=("none" "50" "75" "100")
+    n_inference_stationss=("50" "75" "100")
+    randomize_stations_persamples=("true" "false")
+    inference_stations_seeds=(43 44 45 46) # Different seeds for inference stations
+    for data_type in "${data_types[@]}"; do
+        for inference_stations_seed in "${inference_stations_seeds[@]}"; do
+            for randomize_stations_persample in "${randomize_stations_persamples[@]}"; do
+                for n_random_stations in "${n_random_stationss[@]}"; do
+                    for n_inference_stations in "${n_inference_stationss[@]}"; do
+                        export variable data_type model orography_as_channel additional_input_variables train_years_range n_random_stations n_inference_stations randomize_stations_persample inference_stations_seed
+                        sbatch --export=All jobsub_metrics.slurm
+                    done
                 done
             done
         done
     done
-done
+fi
 
 if false; then  
     model="UNet"
