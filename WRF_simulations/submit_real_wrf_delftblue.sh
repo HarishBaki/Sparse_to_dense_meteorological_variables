db957510bdd5bcfa00ace49fd298ecd37f3f4cbd
HarishBaki <harish.iiitn@gmail.com>
2025-06-06 01:21:59 +0200

    Refactor WRF simulation scripts for improved task allocation and updated module versions

--- WRF_simulations/submit_real_wrf_delftblue.sh
+++ WRF_simulations/submit_real_wrf_delftblue.sh
@@ -3,20 +3,16 @@
 #SBATCH --job-name="case_name"
 #SBATCH --partition=compute
 #SBATCH --time=120:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=48
+#SBATCH --ntasks=96
+#sbatch --cpus-per-task=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=research-ceg-grs
 
-module load 2022r2
-module load openmpi
+module load 2023r1
+module load openmpi/4.1.4
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-
-Udrive_dir="$1"
-case="$2"
-run_dir="$3"
 
 srun ./real.exe	#Executing real.exe
 if ls rsl.out.0000 1>/dev/null 2>&1; #Checking the existance of rsl.out.0000 file
@@ -26,12 +22,12 @@ then
 		#rm met_em.d0*	#Removing the metfiles
 		srun ./wrf.exe	#Executing wrf.exe
 		if tail -n 1 rsl.out.0000 | grep -q "wrf: SUCCESS COMPLETE WRF"; then
-			bash rclone_copy_wrfout_to_HBaki_sftp.sh "$Udrive_dir" "$case" "$run_dir"
+			echo "WRF finished" > wrf.out
 		else
-			echo "WRF $case $run_dir unfinished" > wrf.out
+			echo "WRF unfinished" > wrf.out
 		fi
 	else
-		echo "REAL $case $run_dir unfinished" > real.out	
+		echo "REAL unfinished" > real.out	
 	fi
 else
 	echo "rsl.out.0000 doesn't exist"	
