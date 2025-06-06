14ca8d13c80f0eee8f1b8f568e5a41a7929c6e52
HarishBaki <harish.iiitn@gmail.com>
2025-06-05 20:46:24 -0400

    Add MaskedCombinedMAEQuantileLoss to loss functions and update inference script

--- run_all_inference.sh
+++ run_all_inference.sh
@@ -20,28 +20,51 @@ if false; then
     done
 fi
 
-models=("DCNN" "UNet" "SwinT2UNet")
-orography_as_channels=("false" "true")
-additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
-weights_seeds=("43" "44" "45" "46")
-train_years_range='2018,2021'
-for weights_seed in "${weights_seeds[@]}"; do
-    for model in "${models[@]}"; do
-        additional_input_variables="none"
-        for orography_as_channel in "${orography_as_channels[@]}"; do
-            export model orography_as_channel additional_input_variables weights_seed
-            sbatch --export=All jobsub_inference.slurm
-        done
+if false; then
+    models=("DCNN" "UNet" "SwinT2UNet")
+    orography_as_channels=("false" "true")
+    additional_input_variabless=("si10" "si10,t2m" "si10,t2m,sh2")
+    weights_seeds=("43" "44" "45" "46")
+    train_years_range='2018,2021'
+    for weights_seed in "${weights_seeds[@]}"; do
+        for model in "${models[@]}"; do
+            additional_input_variables="none"
+            for orography_as_channel in "${orography_as_channels[@]}"; do
+                export model orography_as_channel additional_input_variables weights_seed
+                sbatch --export=All jobsub_inference.slurm
+            done
 
-        orography_as_channel="true"
-        for additional_input_variables in "${additional_input_variabless[@]}"; do
-            # Skip condition: UNet with si10,t2m,sh2
-            if [[ "$model" == "UNet" && "$additional_input_variables" == "si10,t2m,sh2" ]]; then
-                echo "Skipping $model with $additional_input_variables"
-                continue
-            fi
-            export model orography_as_channel additional_input_variables weights_seed
-            sbatch --export=All jobsub_inference.slurm
+            orography_as_channel="true"
+            for additional_input_variables in "${additional_input_variabless[@]}"; do
+                # Skip condition: UNet with si10,t2m,sh2
+                if [[ "$model" == "UNet" && "$additional_input_variables" == "si10,t2m,sh2" ]]; then
+                    echo "Skipping $model with $additional_input_variables"
+                    continue
+                fi
+                export model orography_as_channel additional_input_variables weights_seed
+                sbatch --export=All jobsub_inference.slurm
+            done
         done
+    done
+fi
+
+losses=("MaskedMSELoss" "MaskedCombinedMAEQuantileLoss")
+n_inference_stationss=("50" "75" "100" "none")
+for loss in "${losses[@]}"; do
+    for n_inference_stations in "${n_inference_stationss[@]}"; do
+        n_random_stations='none'  # Set to 'none' for inference
+        randomize_stations_persample='false'  # Set to 'true' for inference
+        export n_random_stations randomize_stations_persample n_inference_stations loss
+        sbatch --export=All jobsub_inference.slurm
+
+        n_random_stations='none'
+        randomize_stations_persample='true'  # Set to 'true' for inference
+        export n_random_stations randomize_stations_persample n_inference_stations loss
+        sbatch --export=All jobsub_inference.slurm
+
+        n_random_stations='75'  # 
+        randomize_stations_persample='true'  # Set to 'true' for inference
+        export n_random_stations randomize_stations_persample n_inference_stations loss
+        sbatch --export=All jobsub_inference.slurm
     done
 done
