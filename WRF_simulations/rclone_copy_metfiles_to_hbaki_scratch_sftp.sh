b7f378475647deeb34e847c48816875a9bcc5890
HarishBaki <harish.iiitn@gmail.com>
2025-06-10 19:20:46 -0400

    Updated rclone_copy_metfiles with - Changing root_dir to pwd - Rclone head from DelftBlue to DGX - case 1

--- WRF_simulations/rclone_copy_metfiles_to_hbaki_scratch_sftp.sh
+++ WRF_simulations/rclone_copy_metfiles_to_hbaki_scratch_sftp.sh
@@ -1,12 +1,12 @@
-root_dir='/media/ssd_2tb_evo/Sparse_to_dense_meteorological_variables/WRF_simulations'
+root_dir=$(pwd)
 
 # Define simulation metadata
 cases=('case_1' 'case_2' 'case_3' 'case_4')  # <-- Define this as per your actual simulation case names
 start_dates=('2023-02-02_12:00:00' '2023-03-25_00:00:00' '2023-04-01_00:00:00' '2023-12-17_06:00:00')
 end_dates=('2023-02-04_00:00:00' '2023-03-26_12:00:00' '2023-04-02_12:00:00' '2023-12-18_18:00:00')
 
-for (( i=0; i<${#cases[@]}; i++ )); # remember, 0 means FLLJ_1
-do
+#for (( i=0; i<${#cases[@]}; i++ )); do
+i=0
     case=${cases[$i]}
     echo $case
     # there are folders in the WPS directory, which are ends with _geogrid. read them
@@ -17,7 +17,8 @@ do
 		WPS_dir="WPS_"$run
         # create the WPS run directory
         run_dir=$root_dir/$case/$WPS_dir
-		rclone copy --progress --transfers 12 $run_dir/metfiles hbaki_scratch_sftp:/scratch/hbaki/Sparse_to_dense_meteorological_variables/WRF_simulations/$case/$WPS_dir/metfiles/
+		#rclone copy --progress --transfers 12 $run_dir/metfiles hbaki_scratch_sftp:/scratch/hbaki/Sparse_to_dense_meteorological_variables/WRF_simulations/$case/$WPS_dir/metfiles/
+    rclone copy --progress --transfers 12 $run_dir/metfiles hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/WRF_simulations/$case/$WPS_dir/metfiles/
     done
-done
+#done
 echo "WPS pipeline completed" 
