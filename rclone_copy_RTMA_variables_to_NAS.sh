552ccc6dcce572d70f716b1a8c432a643898d62b
HarishBaki <harish.iiitn@gmail.com>
2025-07-08 11:27:42 -0400

    RTMA data has been transfered to NAS.

--- rclone_copy_RTMA_variables_to_NAS.sh
+++ rclone_copy_RTMA_variables_to_NAS.sh
@@ -1,4 +1,4 @@
-VARIABLES=("WDIR" "SPFH") 
+VARIABLES=("WDIR" "DPT" "PRES"  "GUST" "WIND" "TMP" "SPFH") 
 for i in "${!VARIABLES[@]}"; do
 	VARIABLE="${VARIABLES[i]}"
     rclone copy --progress --transfers 8 "/data/harish/RTMA/$VARIABLE" "harish_NAS:/data/RTMA/$VARIABLE"
