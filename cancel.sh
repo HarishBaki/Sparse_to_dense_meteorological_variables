f7614111a7ed62a7414a21acb179bfd4e812cdbd
HarishBaki <harish.iiitn@gmail.com>
2025-07-09 20:02:34 -0400

    Updated cancel.sh to ddp_train

--- cancel.sh
+++ cancel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-squeue -u $USER --noheader --format="%i %j %T" | awk '$2=="metrics_test" && $3=="PENDING" {print $1}' | while read jobid; do
+squeue -u $USER --noheader --format="%i %j %T" | awk '$2=="ddp_train" && $3=="PENDING" {print $1}' | while read jobid; do
     echo "Cancelling pending job ID: $jobid"
     scancel "$jobid"
 done
 
-squeue -u $USER --noheader --format="%i %j %T" | awk '$2=="metrics_test" && $3=="RUNNING" {print $1}' | while read jobid; do
+squeue -u $USER --noheader --format="%i %j %T" | awk '$2=="ddp_train" && $3=="RUNNING" {print $1}' | while read jobid; do
     echo "Cancelling running job ID: $jobid"
     scancel "$jobid"
 done
