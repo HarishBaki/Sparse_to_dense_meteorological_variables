0c9881ef7824ca032256dfa57c4f19d81a6e1b09
HarishBaki <harish.iiitn@gmail.com>
2025-07-12 02:17:39 -0400

    Added lines to rclone copy trained models in checkpoints.

--- rclone_copy_checkpoints_from_and_to_dgx.sh
+++ rclone_copy_checkpoints_from_and_to_dgx.sh
@@ -1,3 +1,8 @@
-rclone copy --progress --transfers 24 hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints ./checkpoints
+#rclone copy --progress --transfers 24 hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints ./checkpoints
 #rclone copy --progress --transfers 24 ./checkpoints hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints
+rclone copy --progress --transfers 24 \
+  --include "best_model.pt" \
+  --include "latest.pt" \
+  ./checkpoints \
+  hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints
  
