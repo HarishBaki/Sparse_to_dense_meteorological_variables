da593ac593159d2b8af2be23c8c9a88cc4328192
HarishBaki <harish.iiitn@gmail.com>
2025-07-15 12:57:57 -0400

    Updated the rclone copy file

--- rclone_copy_checkpoints_from_and_to_dgx.sh
+++ rclone_copy_checkpoints_from_and_to_dgx.sh
@@ -1,8 +1,10 @@
 #rclone copy --progress --transfers 24 hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints ./checkpoints
 #rclone copy --progress --transfers 24 ./checkpoints hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints
+  #--include "**/NYSM_test_metrics.zarr/**" \
+  #--include "**/RTMA_test_metrics.zarr/**" \
 rclone copy --progress --transfers 24 \
-  --include "best_model.pt" \
-  --include "latest.pt" \
+  --include "**/RTMA_test_metrics.nc" \
+  --include "**/NYSM_test_metrics.nc" \
   ./checkpoints \
-  hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/checkpoints
+  hb533188_dgxhead:/network/rit/dgx/dgx_basulab/Harish/Sparse_to_dense_meteorological_variables/Predictions
  
